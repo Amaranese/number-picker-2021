d3286dadd005fd588ef7ec16f40295801907ef79
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-05 23:51:06 +0200

    ,

---
+++ sample/src/main/java/com/shawnlin/numberpicker/sample/MainActivity.java
@@ -0,0 +1,117 @@
+package com.shawnlin.numberpicker.sample;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import com.shawnlin.numberpicker.NumberPicker;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "NumberPicker";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final NumberPicker numberPicker = findViewById(R.id.number_picker);
+
+        // Set divider color
+        numberPicker.setDividerColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        numberPicker.setDividerColorResource(R.color.colorPrimary);
+
+        // Set formatter
+        numberPicker.setFormatter(getString(R.string.number_picker_formatter));
+        numberPicker.setFormatter(R.string.number_picker_formatter);
+
+        // Set selected text color
+        numberPicker.setSelectedTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        numberPicker.setSelectedTextColorResource(R.color.colorPrimary);
+
+        // Set selected text size
+        numberPicker.setSelectedTextSize(getResources().getDimension(R.dimen.selected_text_size));
+        numberPicker.setSelectedTextSize(R.dimen.selected_text_size);
+
+        // Set selected typeface
+        numberPicker.setSelectedTypeface(Typeface.create(getString(R.string.roboto_light), Typeface.NORMAL));
+        numberPicker.setSelectedTypeface(getString(R.string.roboto_light), Typeface.NORMAL);
+        numberPicker.setSelectedTypeface(getString(R.string.roboto_light));
+        numberPicker.setSelectedTypeface(R.string.roboto_light, Typeface.NORMAL);
+        numberPicker.setSelectedTypeface(R.string.roboto_light);
+
+        // Set text color
+        numberPicker.setTextColor(ContextCompat.getColor(this, R.color.dark_grey));
+        numberPicker.setTextColorResource(R.color.dark_grey);
+
+        // Set text size
+        numberPicker.setTextSize(getResources().getDimension(R.dimen.text_size));
+        numberPicker.setTextSize(R.dimen.text_size);
+
+        // Set typeface
+        numberPicker.setTypeface(Typeface.create(getString(R.string.roboto_light), Typeface.NORMAL));
+        numberPicker.setTypeface(getString(R.string.roboto_light), Typeface.NORMAL);
+        numberPicker.setTypeface(getString(R.string.roboto_light));
+        numberPicker.setTypeface(R.string.roboto_light, Typeface.NORMAL);
+        numberPicker.setTypeface(R.string.roboto_light);
+
+        // Set value
+        numberPicker.setMaxValue(59);
+        numberPicker.setMinValue(0);
+        numberPicker.setValue(3);
+
+        // Set string values
+//        String[] data = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+//        numberPicker.setMinValue(1);
+//        numberPicker.setMaxValue(data.length);
+//        numberPicker.setDisplayedValues(data);
+
+        // Set fading edge enabled
+        numberPicker.setFadingEdgeEnabled(true);
+
+        // Set scroller enabled
+        numberPicker.setScrollerEnabled(true);
+
+        // Set wrap selector wheel
+        numberPicker.setWrapSelectorWheel(true);
+
+        // Set accessibility description enabled
+        numberPicker.setAccessibilityDescriptionEnabled(true);
+
+        // OnClickListener
+        numberPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Click on current value");
+            }
+        });
+
+        // OnValueChangeListener
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                Log.d(TAG, String.format(Locale.US, "oldVal: %d, newVal: %d", oldVal, newVal));
+            }
+        });
+
+        // OnScrollListener
+        numberPicker.setOnScrollListener(new NumberPicker.OnScrollListener() {
+            @Override
+            public void onScrollStateChange(NumberPicker picker, int scrollState) {
+                if (scrollState == SCROLL_STATE_IDLE) {
+                    Log.d(TAG, String.format(Locale.US, "newVal: %d", picker.getValue()));
+                }
+            }
+        });
+    }
+
+}
